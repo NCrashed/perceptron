c8c4f6383b116fdeceaacecddca732c5bc3b4552
NCrashed <NCrashed@gmail.com>
2014-05-25 18:20:47 +0400

    Application options

--- source/app.d
+++ source/app.d
@@ -12,7 +12,47 @@
  */
 module app;
 
-void main()
-{
+import std.stdio;
+import std.getopt;
 
+enum helpMsg = 
+"perceptron [args]
+
+args: --learning    - defines learning mode for neural network
+      --recognition - defines recognition mode for neural networ.
+                      default flag.
+      --config=path - configuration file, default is 'config.json'.
+";
+
+void main(string[] args)
+{
+    bool learning = false;
+    bool recognition = true;
+    bool help = false;
+    string configPath = "config.json";
+    
+    {
+        scope(failure)
+        {
+            writeln(helpMsg);
+            return;
+        }
+        
+        getopt(args,
+            "learning", &learning,
+            "recoginition", &recognition,
+            "config", &configPath,
+            "help", &help
+        );
+        
+        assert(learning != recognition, "Cannot use learning and recognition at the same time!");
+        
+        if(help)
+        {
+            writeln(helpMsg);
+            return;
+        }
+    }
+    
+    
 }
