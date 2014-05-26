084a74370f2a0f3979921dd2da3022a1922c2d4d
NCrashed <NCrashed@gmail.com>
2014-05-26 16:15:20 +0400

    C letter

--- source/app.d
+++ source/app.d
@@ -94,7 +94,7 @@ void main(string[] args)
             writeln("symbol: ", sample.answer);
         } 
         
-        alias TestNet = Perceptron!(INPUT_SIZE, INPUT_SIZE*INPUT_SIZE, INPUT_SIZE*2, 2);
+        alias TestNet = Perceptron!(INPUT_SIZE, INPUT_SIZE*INPUT_SIZE, INPUT_SIZE*2, 3);
         TestNet testNet;
         testNet.randomInit;
         
